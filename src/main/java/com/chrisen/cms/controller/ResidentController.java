0343a57f0f6a1012b8531f09cfba7ddf53533ce5
Chrisen Galang <chrisen.galang@gmail.com>
2022-08-21 00:35:10 +0800

    Code cleanup

--- src/main/java/com/chrisen/cms/controller/ResidentController.java
+++ src/main/java/com/chrisen/cms/controller/ResidentController.java
@@ -1,7 +1,5 @@
 package com.chrisen.cms.controller;
 
-import java.util.List;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
